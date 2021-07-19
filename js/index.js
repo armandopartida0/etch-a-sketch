8ff8aa74f6c211ecd810d1bec1615356d82508ed
Armando Partida Sanabia <armandopartida0@outlook.com>
2021-07-19 00:33:38 -0400

    Refactor and add generateGrid() function

--- js/index.js
+++ js/index.js
@@ -1,14 +1,8 @@
 // Grab our grid container node
 const gridContainer = document.querySelector('.grid-container');
 
-// Create our grid layout on page load
-for(let i = 0; i < 16; i++) {
-    for(let j = 0; j < 16; j++) {
-        const div = document.createElement('div');
-        div.className = 'square';
-        gridContainer.append(div);
-    }
-}
+// Create our grid layout on page load. 16x16 intial size
+generateGrid(16);
 
 // Add event listeners to squares to change color
 const squares = document.querySelectorAll('.square');
@@ -17,6 +11,39 @@ squares.forEach(item => {
         e.target.style.backgroundColor = randomColor();
     });
 });
+
+// Add event listener to button and check for user input for new grid
+const resetBtn = document.querySelector('.etch-bottom-btn');
+resetBtn.addEventListener('click', () => {
+    let answer = prompt('Enter new grid size (1-100)');
+
+    if(answer != null) {
+        while(true) {
+            if(!(answer > 0 && answer <= 100)) {
+                answer = prompt('Not a valid option. Enter new grid size (1-100)');
+            } else {
+                break;
+            }
+        }
+
+        // Destroy grid and generate new grid
+        // TODO
+    }
+});
+
+/**
+ * Function to generate a grid
+ * @param {number} n Specified number to generate a n x n grid
+ */
+function generateGrid(n) {
+    for(let i = 0; i < n; i++) {
+        for(let j = 0; j < n; j++) {
+            const div = document.createElement('div');
+            div.className = 'square';
+            gridContainer.append(div);
+        }
+    }
+}
 
 /**
  * Helper function to return a random color
